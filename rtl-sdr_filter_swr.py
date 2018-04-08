9ebb0f95dd92cfb1b21ee440c13c7f8258968a6e
Dennis Koole <dennis.koole@gmail.com>
2018-04-08 23:10:46 +0200

    csv generation for SWR measurement

--- rtl-sdr_filter_swr.py
+++ rtl-sdr_filter_swr.py
@@ -2,67 +2,97 @@ import matplotlib.pyplot as plt
 import subprocess
 import csv
 
-reference_file = "reference.csv"
+filter_baseline_file = "reference.csv"
 filter_file = "filter.csv"
-output_file = "output.csv"
+filter_output_file = "output.csv"
+swr_baseline_file = "swr_baseline.csv"
+swr_antenna_file = "swr_antenna.csv"
+swr_output_file = "swr.csv"
 
 if __name__ == "__main__":
-	print "Script to measure Filters and VSWR with rtl-sdr dongle"
-	measurement_name = raw_input("Name for measurement")
+	print "Script to measure filter characteristics and antenna VSWR with rtl-sdr dongle"
+	measurement_name = raw_input("Name for measurement ")
 	startFrequency = input("Enter start frequency in MegaHertz ")
 	stopFrequency  = input("Enter stop frequenc in MegaHertz ")
+	#measurementType = raw_input("Measurement type filter/swr ")
+	measurementType = "swr"
 
-	x = raw_input("Press Enter to start Baseline measurement ")
+	if measurementType.lower() == "filter" :
+		print "Selected Filter characteristics measurement"
+		reference_file = filter_baseline_file
+		connected_file = filter_file
+	else :
+		print "Selected Antenna SWR measurement"
+		reference_file = swr_baseline_file
+		connected_file = swr_antenna_file
 
-	subprocess.call(["rtl_power", "-f", str(startFrequency) + "M:" + str(stopFrequency) + "M:1M", "-i", "10", "-1", "-g", "0", reference_file])
-	x  = raw_input("Press Enter to start next measurement")
-	subprocess.call(["rtl_power", "-f", str(startFrequency) + "M:" + str(stopFrequency) + "M:1M", "-i", "10", "-1", "-g", "0", filter_file])
+	# x = raw_input("Press Enter to start Baseline measurement ")
 
-	reference = []
+	# subprocess.call(["rtl_power", "-f", str(startFrequency) + "M:" + str(stopFrequency) + "M:1M", "-i", "10", "-1", "-g", "0", reference_file])
+	# x  = raw_input("Press Enter to start next measurement")
+	# subprocess.call(["rtl_power", "-f", str(startFrequency) + "M:" + str(stopFrequency) + "M:1M", "-i", "10", "-1", "-g", "0", connected_file])
+
+	reference_power = []
 	frequencies_start = []
 	frequencies_stop = []
-	filter_list = []
-	reference_list = []
-	attenuation = []
+	connected_power = []
+	# reference_list = []
+	# attenuation = []
 
 	with open(reference_file, 'rb') as ref_file:
 		reader = csv.reader(ref_file, delimiter=",",quoting=csv.QUOTE_NONE)
 		for row in reader:
-			reference.append(row[6])
+			reference_power.append(row[6])
 			frequencies_start.append(row[2])
 			frequencies_stop.append(row[3])
 
-	with open(filter_file, 'rb') as filt_file:
+	with open(connected_file, 'rb') as filt_file:
 	 	reader = csv.reader(filt_file, delimiter=",",quoting=csv.QUOTE_NONE) 
-		filter_list = [row[6] for row in reader]
+		connected_power = [row[6] for row in reader]
 
-	with open(output_file, 'w') as csvfile:
-	    fieldnames = ['Frequency', '', 'Baseline dBm', 'Filter dBm', "Attenuation"]
-	    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	with open(swr_output_file, 'w') as csvfile:
+		fieldnames = ['Frequency', '', 'Baseline', 'Antenna connected', 'Return Loss', 'Adjusted RL', 'SWR']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-	    writer.writeheader()
+		writer.writeheader()
 
-	    for i in xrange(len(reference)):
-	    	attenuation.append(float(filter_list[i]) - float(reference[i]))
-	    	writer.writerow({'Frequency': frequencies_start[i], '': frequencies_stop[i], 'Baseline dBm': reference[i], 'Filter dBm': filter_list[i], 'Attenuation' : float(filter_list[i]) - float(reference[i])})
+		return_loss = []
+		return_loss_corrected = []
 
-	plt.figure(1)
-	line_filter, = plt.plot(frequencies_start, filter_list, linewidth=3.0, label=measurement_name)
-	line_reference, = plt.plot(frequencies_start, reference, linewidth=3.0, label="Baseline")
-	plt.title(measurement_name, fontweight='bold')
-	plt.grid(True, linestyle='-')
-	plt.legend(handles=[line_reference, line_filter])
-	plt.ylabel('Power (dB)', fontweight='bold')
-	plt.xlabel('Frequency (Hz)', fontweight='bold')
-	plt.savefig("TestTest")
+		for i in xrange(len(reference_power)):
+			return_loss.append(float(connected_power[i]) - float(reference_power[i]))
+			return_loss_corrected.append(max(0.0001, return_loss[i]))
+			base_calc = 10 * (return_loss_corrected[i] / 20)
+			vswr = ((base_calc + 1) / (base_calc - 1))
+			writer.writerow({'Frequency': frequencies_start[i], '': frequencies_stop[i], 'Baseline' : reference_power[i], 'Antenna connected' : connected_power[i], 'Return Loss' : return_loss[i], 'Adjusted RL' : return_loss_corrected[i], "SWR" : vswr})
 
-	plt.figure(2)
-	attenuation_plot, = plt.plot(frequencies_start, attenuation, linewidth=3.0, label="Attenuation")
-	plt.title(measurement_name, fontweight='bold')
-	plt.grid(True, linestyle='-')
-	plt.legend(handles=[attenuation_plot])
-	plt.ylabel('Attenuation (dB)', fontweight='bold')
-	plt.xlabel('Frequency (Hz)', fontweight='bold')
-	plt.savefig("Attenuation")
+	# with open(output_file, 'w') as csvfile:
+	#     fieldnames = ['Frequency', '', 'Baseline dBm', 'Filter dBm', "Attenuation"]
+	#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-	plt.show()
+	#     writer.writeheader()
+
+	#     for i in xrange(len(reference)):
+	#     	attenuation.append(float(filter_list[i]) - float(reference[i]))
+	#     	writer.writerow({'Frequency': frequencies_start[i], '': frequencies_stop[i], 'Baseline dBm': reference[i], 'Filter dBm': filter_list[i], 'Attenuation' : float(filter_list[i]) - float(reference[i])})
+
+	# plt.figure(1)
+	# line_filter, = plt.plot(frequencies_start, filter_list, linewidth=3.0, label=measurement_name)
+	# line_reference, = plt.plot(frequencies_start, reference, linewidth=3.0, label="Baseline")
+	# plt.title(measurement_name, fontweight='bold')
+	# plt.grid(True, linestyle='-')
+	# plt.legend(handles=[line_reference, line_filter])
+	# plt.ylabel('Power (dB)', fontweight='bold')
+	# plt.xlabel('Frequency (Hz)', fontweight='bold')
+	# plt.savefig("TestTest")
+
+	# plt.figure(2)
+	# attenuation_plot, = plt.plot(frequencies_start, attenuation, linewidth=3.0, label="Attenuation")
+	# plt.title(measurement_name, fontweight='bold')
+	# plt.grid(True, linestyle='-')
+	# plt.legend(handles=[attenuation_plot])
+	# plt.ylabel('Attenuation (dB)', fontweight='bold')
+	# plt.xlabel('Frequency (Hz)', fontweight='bold')
+	# plt.savefig("Attenuation")
+
+	# plt.show()
